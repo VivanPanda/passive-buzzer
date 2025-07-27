b6eca3f1f7f91d81fec0e8ab31db823c5f3b88a7
VivanPanda <vivanpanda13@gmail.com>
2025-07-27 14:38:49 +0400

    setup potentiometer

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,18 @@
+int buzzerPin = 7;
+int potPin = A7;
+int readVal;
+int delayTime;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(potPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  readVal = analogRead(potPin);
+  Serial.println(readVal);
+  delay(500);
 }
